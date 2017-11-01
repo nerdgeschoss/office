cec7b5d1b1ab33d33563ffcfe92d9b828e9bd2a0
Jens Ravens <jens@nerdgeschoss.de>
2017-11-01 15:26:16 +0100

    scope financial access  to companies

--- app/policies/team_policy.rb
+++ app/policies/team_policy.rb
@@ -3,6 +3,18 @@ class TeamPolicy < ApplicationPolicy
     true
   end
 
+  def show_financial?
+    admin? || member?
+  end
+
+  def financial_overview?
+    admin?
+  end
+
+  def member?
+    user.team_id == resource.id
+  end
+
   class Scope < Scope
     def resolve
       scope.all
