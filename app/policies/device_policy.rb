0079eaf225a7eecf236ad52f73901a626a1f3f31
Jens Ravens <jens@nerdgeschoss.de>
2017-11-20 11:57:29 +0100

    activate device feature for regular users

--- app/policies/device_policy.rb
+++ app/policies/device_policy.rb
@@ -1,2 +1,19 @@
 class DevicePolicy < ApplicationPolicy
+  def create?
+    true
+  end
+
+  def update?
+    admin? || owner?
+  end
+
+  def destroy?
+    admin? || owner?
+  end
+
+  private
+
+  def owner?
+    resource.user == user
+  end
 end
